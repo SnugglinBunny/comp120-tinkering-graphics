c2fe51ee043beca20334c631f40377338fc95966
StevenCowie <steven.cowie@hotmail.co.uk>
2016-10-26 11:59:58 +0100

    Displays Glasses on Face

--- BillNye.py
+++ BillNye.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 pygame.init()
 
 DISPLAY = pygame.display.set_mode((1600, 1066), 0, 32)
+pygame.display.set_caption('Bill Nye')
 
 BLACK = (0, 0, 0)
 WHITE= (255, 255, 255) # red, green, blue in 8-bits
@@ -11,7 +12,7 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-billnye = pygame.image.load('C:\Users\SC191991\downloads\BillNye.jpg')
+billnye = pygame.image.load('images\BillNye.jpg')
 imagerect = billnye.get_rect()
 
 def drawNye():
@@ -20,9 +21,28 @@ def drawNye():
 
 drawNye()
 
+glasses = pygame.image.load('images\glasses.png')
+
+def drawGlasses():
+    DISPLAY.blit(glasses, (670, 120))
+    pygame.display.flip()
+
+def gunOne():
+    pxarray = pygame.PixelArray(DISPLAY)
+    pxarray [40:200, 200:250] = (254,70,5)
+
+
+
 while True:
    for event in pygame.event.get():
        if event.type == QUIT:
            pygame.quit()
            sys.exit()
-   pygame.display.update()
+   if event.type == pygame.KEYDOWN:
+       if event.key == pygame.K_g:
+           drawGlasses()
+   if event.type == pygame.KEYDOWN:
+       if event.key == pygame.K_h:
+           gunOne()
+
+pygame.display.update()
