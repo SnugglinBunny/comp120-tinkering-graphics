6d7ac18975882fa307111a95cde5fbd624fda83b
StevenCowie <steven.cowie@hotmail.co.uk>
2016-10-26 09:35:11 +0100

    Added code - Displays Image

--- BillNye.py
+++ BillNye.py
@@ -5,12 +5,20 @@ pygame.init()
 
 DISPLAY = pygame.display.set_mode((1600, 1066), 0, 32)
 
+BLACK = (0, 0, 0)
+WHITE= (255, 255, 255) # red, green, blue in 8-bits
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
 billnye = pygame.image.load('C:\Users\SC191991\downloads\BillNye.jpg')
 imagerect = billnye.get_rect()
 
 def drawNye():
     DISPLAY.blit(billnye, imagerect)
     pygame.display.flip()
+
+drawNye()
 
 while True:
    for event in pygame.event.get():
