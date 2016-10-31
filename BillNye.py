a47998db3d3574b94b469359200f9d8e27c430e7
rasta_panda@hotmail.co.uk <Bethany7>
2016-10-31 17:26:50 +0000

    Fixing file locations and adding Greyscale/BW

--- BillNye.py
+++ BillNye.py
@@ -1,35 +1,60 @@
-import pygame, sys
+import pygame, sys, PIL
 from pygame.locals import *
-
+from pygame.image import save
+from PIL import Image
 pygame.init()
 
-DISPLAY = pygame.display.set_mode((1600, 1066), 0, 32)
-pygame.display.set_caption('Bill Nye')
+DISPLAY = pygame.display.set_mode((1400, 932), 0, 32)
+pygame.display.set_caption('images\Bill Nye')
+clock = pygame.time.Clock()
+colourImage = pygame.image.load('images\BillNye.jpg')
+delay = 200
+red = False
+green = True
+blue = False
 
-BLACK = (0, 0, 0)
-WHITE= (255, 255, 255) # red, green, blue in 8-bits
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
-billnye = pygame.image.load('images\BillNye.jpg')
-imagerect = billnye.get_rect()
-
-def drawNye():
-    DISPLAY.blit(billnye, imagerect)
+def drawimage():
+    DISPLAY.blit(colourImage, [0, 0])
+    pygame.time.delay(delay)
+    pygame.display.flip()
+    DISPLAY.blit(colourImage, [700, 0])
+    pygame.time.delay(delay)
+    pygame.display.flip()
+    DISPLAY.blit(colourImage, [0, 466])
+    pygame.time.delay(delay)
+    pygame.display.flip()
+    DISPLAY.blit(colourImage, [700, 466])
+    pygame.time.delay(delay)
     pygame.display.flip()
 
-drawNye()
-
-glasses = pygame.image.load('images\glasses.png')
-
-def drawGlasses():
-    DISPLAY.blit(glasses, (670, 120))
+def blite():
+    image = Image.open('images\BillNye.jpg')
+    gray = image.convert('L')
+    bw = gray.point(lambda x: 0 if x < 128 else 255, '1')
+    bw.save('images\BillNyeBW.png')
+    bwimage = pygame.image.load('images\BillNyeBW.png')
+    DISPLAY.blit(bwimage, [0, 0])
+    pygame.time.delay(delay)
+    pygame.display.flip()
+    DISPLAY.blit(bwimage, [700, 0])
+    pygame.time.delay(delay)
+    pygame.display.flip()
+    DISPLAY.blit(bwimage, [0, 466])
+    pygame.time.delay(delay)
+    pygame.display.flip()
+    DISPLAY.blit(bwimage, [700, 466])
+    pygame.time.delay(delay)
     pygame.display.flip()
 
-def gunOne():
-    pxarray = pygame.PixelArray(DISPLAY)
-    pxarray [40:200, 200:250] = (254,70,5)
+def greyscale():
+    for x in range(colourImage.get_width()):
+        for y in range(colourImage.get_height()):
+            c = list(colourImage.get_at((x,y))[:3])
+            deg = (c[0]*red +c[1]*green + c[2]*blue) / (red + green + blue)
+
+            colourImage.fill( [deg]*3, ((x,y), (1,1)))
+
 
 
 
@@ -39,10 +64,14 @@ while True:
            pygame.quit()
            sys.exit()
    if event.type == pygame.KEYDOWN:
-       if event.key == pygame.K_g:
-           drawGlasses()
+       if event.key == pygame.K_d:
+           drawimage()
    if event.type == pygame.KEYDOWN:
-       if event.key == pygame.K_h:
-           gunOne()
+       if event.key == pygame.K_g:
+           blite()
+   if event.type == pygame.KEYDOWN:
+       if event.key == pygame.K_t:
+            greyscale()
 
 pygame.display.update()
+clock.tick(60)
